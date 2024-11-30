94f770b4950d005107e869347279c5deffadc72e
ISHAN CHAKRABORTY <109605317+kujohIshan@users.noreply.github.com>
2024-11-30 10:24:13 +0530

    Time: 27 ms (15.46%), Space: 14.3 MB (38.65%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -8,16 +8,30 @@ public:
         }
         // brute force generate all substrings
         
-        for(int i =0; i<n; i++){
-            unordered_set<char> present;
-            for(int j=i;j<n;j++){
-                if(present.count(s[j])) // if the char is present break 
-                break;
+        // for(int i =0; i<n; i++){
+        //     unordered_set<char> present;
+        //     for(int j=i;j<n;j++){
+        //         if(present.count(s[j])) // if the char is present break 
+        //         break;
             
-            // else insert 
-            present.insert(s[j]);
-            maxsize= max(maxsize, j-i+1);
+        //     // else insert 
+        //     present.insert(s[j]);
+        //     maxsize= max(maxsize, j-i+1);
+        //     }
+        // }
+        // return maxsize;
+
+
+        // optimal using 2 pointers
+        int left =0;
+        unordered_set<char> present;
+        for(int right=0;right<n;right++){
+            while(present.count(s[right])){ // if duplicate is present it will erase the first element and start from the next
+                present.erase(s[left]);
+                left++;
             }
+            present.insert(s[right]);
+            maxsize= max(maxsize , right -left +1);
         }
         return maxsize;
     }
