9d7c4cf0d3aef5467a29c8f38add9a7f72672560
ISHAN CHAKRABORTY <109605317+kujohIshan@users.noreply.github.com>
2024-12-01 11:28:05 +0530

    Time: 0 ms (100%), Space: 32.7 MB (39.89%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -1,20 +1,44 @@
 class Solution {
 public:
+    int func(vector<int>& nums, int goal){
+        if(goal< 0){
+            return 0;
+        }
+        int left =0 ,right =0, sum=0,count=0;
+        int n =nums.size();
+        while(right< n){
+            sum= sum+nums[right];
+            while(sum >goal){
+                sum -= nums[left];
+                left =left +1;
+            }
+            count = count +(right - left +1);
+            right = right +1;
+        }
+        return count;
+    }
     int numSubarraysWithSum(vector<int>& nums, int goal) {
         int n =nums.size();
         int count=0;
-        for(int i=0;i<n;i++){
-            int sum =0;
-            for(int j=i;j<n;j++){
-                sum+=nums[j];
-                if(sum==goal){
-                    count++;
-                }
-                else if(sum > goal){
-                    break;
-                }
-            }
-        }
-        return count;
+
+        // brute force
+
+        // for(int i=0;i<n;i++){
+        //     int sum =0;
+        //     for(int j=i;j<n;j++){
+        //         sum+=nums[j];
+        //         if(sum==goal){
+        //             count++;
+        //         }
+        //         else if(sum > goal){
+        //             break;
+        //         }
+        //     }
+        // }
+        // return count;
+
+        // optimal 
+        
+        return func(nums, goal) - func(nums, goal -1);
     }
 };
